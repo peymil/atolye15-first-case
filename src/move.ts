01cbefe224b382e12f08797210be1b3c829af7e8
Kenny <emrebadem_@hotmail.com>
2021-12-03 04:07:39 +0300

    foreach

--- src/move.ts
+++ src/move.ts
@@ -1,6 +1,3 @@
-// ESLint giving regenerator-runtime error because of regenerator-runtime's poor transpiled performance but this code is not going to work on browsers. So i think we can ignore it.
-// If you are asking about why I'm insisting about for-of loop: Handling throwed exceptions inside foreach-map loop is hard and we are not able to break foreach-map loops.
-
 type Folder = {
   id: string;
   name: string;
@@ -13,19 +10,17 @@ type File = {
 type List = Folder[];
 
 export default function move(list: List, source: string, destination: string): List {
-  // Editing a referenced object(or array) is a little bit dangerous so i'm not going to edit it directly instead i cloned it to a new variable.
   const newList = [...list];
   let sourceFile: File | undefined;
   let destFolderIndex: number | undefined;
-  for (const [folderN, folder] of list.entries()) {
+  list.forEach((folder, folderN) => {
     if (folder.id === source) throw new Error('You cannot move a folder');
-    for (const [fileN, file] of folder.files.entries()) {
+    folder.files.forEach((file, fileN) => {
       if (file.id === destination) throw new Error('You cannot specify a file as the destination');
       if (file.id === source) [sourceFile] = newList[folderN].files.splice(fileN, 1);
-    }
+    });
     if (folder.id === destination) destFolderIndex = folderN;
-    if (sourceFile && destFolderIndex) break;
-  }
+  });
   if (!sourceFile) throw new Error('Source file is not found');
   if (!destFolderIndex) throw new Error('Destination folder is not found');
   newList[destFolderIndex].files.push(sourceFile);
